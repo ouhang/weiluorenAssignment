89bea1701801a7313afeb4011ed4a0c398f9e297
ouhang <ouhang1989@gmail.com>
2015-04-16 20:38:41 -0700

    Update CurrentTime.java

--- multithread/currentTime/CurrentTime.java
+++ multithread/currentTime/CurrentTime.java
@@ -4,13 +4,13 @@ package currentTime;
  * Interface used to test elapsed time in the programs.
  * 
  * @author weiluo
- * 
+ * ########################Delete extra useless lines or spaces like this line, here and anywhere else
  */
 public interface CurrentTime {
   /**
    * In real implementation, it should return the "real" current time, while in
    * the test, it might return some "fake" time.
-   * 
+   * ########################You don't need to say a lot about it's implements like test / real stuff
    * @return current time in milli-second
    */
   public long NowMillis();
